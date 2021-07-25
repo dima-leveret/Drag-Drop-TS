fa05e26d6c77389af572b632b6cf42eed02000f1
Dima <dimazayac@gmail.com>
2021-07-25 18:39:03 +0200

    reconstruced the app using ES modules

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,18 +1,14 @@
 // Drag & Drop Interfaces
-namespace App {
-  export interface Draggable {
-    dragStartHamdler(event: DragEvent): void;
+export interface Draggable {
+  dragStartHamdler(event: DragEvent): void;
 
-    dragEndHandler(event: DragEvent): void;
-  }
-
-  export interface DtagTarget {
-    dragOverHamdler(event: DragEvent): void;
-
-    dropHandler(event: DragEvent): void;
-
-    dragLeaveHandler(event: DragEvent): void;
-  }
+  dragEndHandler(event: DragEvent): void;
 }
 
+export interface DtagTarget {
+  dragOverHamdler(event: DragEvent): void;
 
+  dropHandler(event: DragEvent): void;
+
+  dragLeaveHandler(event: DragEvent): void;
+}
